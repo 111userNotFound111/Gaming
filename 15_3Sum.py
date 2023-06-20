872bbf1f0eaf4f1cd3fa767aea87a2666cdc1c50
ray <raymondf0123@gmail.com>
2023-06-20 17:39:04 +1000

    "need to fix bug "

---
+++ 15_3Sum.py
@@ -0,0 +1,24 @@
+
+def threeSum(nums):
+        nums.sort()
+        result = []
+        for index, num in enumerate(nums):
+            if index != 0 and num == nums[index - 1]:
+                continue
+            
+            left = index + 1 
+            right = len(nums) - 1 
+            
+            while left < right:
+                sum = num + nums[left] + nums[right]
+                
+                if sum < 0 :
+                    left = left + 1
+                if sum > 0 :
+                    right = right - 1 
+                else : 
+                    result.append([num, nums[left], nums[right]])
+                    left += 1
+                    while nums[left] == nums[left -1] and left < right:
+                        left += 1
+        return result
