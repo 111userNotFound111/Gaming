c87fb97819a7bd32443d1f5d48ab4fdb662541cb
ray <raymondf0123@gmail.com>
2023-09-16 21:02:40 +1000

    238 improve

--- 238_Product_of_Array_Except_Self.py
+++ 238_Product_of_Array_Except_Self.py
@@ -10,27 +10,59 @@ You must write an algorithm that runs in O(n) time
 and without using the division operation.
 
 """
+
 from collections import defaultdict
+
 def productExceptSelf(nums):
     # product = prefix product * surfix product
-    # prefix 
-    prefix_dict={}
-    # surfix 
-    surfix_dict={}
-    # product 
-    product_dict={}
 
-    # get prefix product for all num in list
-    for index in range (0,len(nums)):
-        product_list=[]
-        for position in range (0, len(nums)):
-            print(position)
-            if position != index:
-                product_list.append(nums[position])
+    # prefix dictionary
+    # struct {position i: prefix product} 
+    prefix_dict={}
+    
+    # initialize postfix product value 
+    postfix_product=1
+    
+    # output list = postfix * prefix  
+    output=[0] * len(nums)
+    
+    # for prefix
+    for i in range(0,len(nums)):
+        prefix_pos=i-1
+        # prefix for first item
+        if prefix_pos < 0:
+            prefix_dict[0] = 1
+        else:
+            # initialize the prefix value of first item = 1 
+            # prefix product [i] = prefix product[i-1] * nums[i-1] 
+            prefix_dict[i]=prefix_dict[prefix_pos] * nums[prefix_pos]
         
-        print(index)
-        print(product_list)
+    # for postfix
+    for i in range(len(nums)-1,-1,-1):
+
+        if i == len(nums)-1:
+            postfix_product=1
+        else:
+            postfix_product = postfix_product * nums[i+1]
+        
+        #print(f'postfix_product: {postfix_product} and prefix product: {prefix_dict[i]}')
+        
+        output[i] = prefix_dict[i] * postfix_product
+    return output
+
+# improve efficiency, lower memory used
+def productExceptSelfEfficient(nums):
+    res=[1] * len(nums)
+    prefix = 1 
+    postfix = 1
+    for i in range(0,len(nums)):
+        res[i]=prefix
+        prefix=prefix * nums[i]
+    for j in range(len(nums)-1,-1,-1):
+        res[j]=res[j]*postfix
+        postfix=postfix * nums[j]
+    return res
 
 if __name__ == "__main__":
-    nums = [1,2,3,4]
-    productExceptSelf(nums)
+    nums = [-1,1,0,-3,3]
+    print(productExceptSelfEfficient(nums))
