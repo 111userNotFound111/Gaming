6592ef9eb08530da2f87a98d24d627b460ff5c34
111userNotFound111 <z5336391@ad.unsw.edu.au>
2023-10-21 11:22:03 +1100

    33 search in rotated sorted array

---
+++ 33_Search_in_Rotated_Sorted_Array.py
@@ -0,0 +1,86 @@
+"""
+
+33. Search in Rotated Sorted Array
+
+There is an integer array nums sorted in ascending order (with distinct values).
+
+Prior to being passed to your function, nums is possibly rotated at an unknown pivot index k (1 <= k < nums.length) such that the resulting array is [nums[k], nums[k+1], ..., nums[n-1], nums[0], nums[1], ..., nums[k-1]] (0-indexed). For example, [0,1,2,4,5,6,7] might be rotated at pivot index 3 and become [4,5,6,7,0,1,2].
+
+Given the array nums after the possible rotation and an integer target, return the index of target if it is in nums, or -1 if it is not in nums.
+
+You must write an algorithm with O(log n) runtime complexity.
+
+ 
+
+Example 1:
+
+Input: nums = [4,5,6,7,0,1,2], target = 0
+Output: 4
+Example 2:
+
+Input: nums = [4,5,6,7,0,1,2], target = 3
+Output: -1
+Example 3:
+
+Input: nums = [1], target = 0
+Output: -1
+ 
+
+Constraints:
+
+1 <= nums.length <= 5000
+-104 <= nums[i] <= 104
+All values of nums are unique.
+nums is an ascending array that is possibly rotated.
+-104 <= target <= 104
+
+
+"""
+def search(nums, target):
+    # initialize left, right pointer 
+    left = 0
+    right = len(nums) -1 
+
+    while left <= right:
+        # calculate mid 
+        mid = (left + right) // 2 
+        # first locate which region target is in 
+        # if target in left region
+        if target > nums[right]:
+            # if mid in the same left region
+            if nums[mid] > nums[right]:
+                # perform binary search on the region
+                # if nums[mid] > target 
+                if nums[mid] > target:
+                    right = mid - 1
+                else :
+                    left = mid + 1
+            
+            # if mid not in same region
+            else :
+                right = mid - 1
+
+        # if target in right region
+        else:
+            # same region
+            if nums[mid] < nums[right]:
+                if nums[mid] > target :
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            # different region
+            else:
+                left = mid + 1
+        
+        print(mid)
+
+        # stop case 
+        if nums[mid] == target:
+            return True
+    
+    return -1 
+
+if __name__ == "__main__":
+    nums = [4,5,6,7,0,1,2]
+    target = 0
+    print(search(nums, target))
--- 704_Binary_Search.py
+++ 704_Binary_Search.py
@@ -1,23 +1,62 @@
-import numpy as np
-import math
+"""
+704. Binary Search
 
-def search( nums, target):
-    # define new variables for left and right
-    n = len(nums)
+Given an array of integers nums which is sorted in ascending order, 
+and an integer target, write a function to search target in nums.
+If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+"""
+# def search( nums, target):
+#     # define new variables for left and right
+#     n = len(nums)
+#     left = 0
+#     right = len(nums) - 1 
+
+#     # loop stops if either left or right is greater 
+#     while left <= right:
+#         # check poisiton of the target value ( left / right )
+#         mid_point = left + ( (right - left ) // 2 )
+#         # if target is at the left side, update the mid point 
+#         if target < nums[mid_point] :
+#             right = mid_point - 1 
+#         elif target > nums[mid_point] :
+#             left = mid_point + 1 
+#         else :
+#             return print(mid_point)
+#         return -1 
+
+# Binary Search
+# assume : the input list is a sorted list 
+# define left, right range and update condition
+# define mid search condition
+# locate and return target 
+
+def search(nums, target):
+    target_index = -1
     left = 0
-    right = len(nums) - 1 
+    right = len(nums)-1
+    print("start")
 
-    # loop stops if either left or right is greater 
-    while left <= right:
-        # check poisiton of the target value ( left / right )
-        mid_point = left + ( (right - left ) // 2 )
-        # if target is at the left side, update the mid point 
-        if target < nums[mid_point] :
-            right = mid_point - 1 
-        elif target > nums[mid_point] :
-            left = mid_point + 1 
-        else :
-            return print(mid_point)
-        return -1 
+    while (left <= right):
+        
+        mid = left + ((right - left)//2)
+        
+        # target at the left 
+        if nums[mid] > target:            
+            right = mid-1
+        # target at the right 
+        elif nums[mid] < target:
+            left = mid+1
+        # nums[mid] == target
+        else:
+            target_index = mid
+            return target_index
 
-search([1,2,3,4,5],2)
+    # default target index -1 
+    return target_index
+
+if __name__ == "__main__":
+    nums = [-1,0,3,5,9,12]
+    target = 9
+    print(search(nums, target))
